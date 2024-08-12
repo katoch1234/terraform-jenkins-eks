4c19c6e22ef8df8f36da7c8edf8da00eac98be86
cu.16bcs1092 <cu.16bcs1092@gmail.com>
2024-08-12 19:37:30 +0530

    updated s3 bucket name

--- eks-tfconf/backend.tf
+++ eks-tfconf/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "jenkins-backend678-kode-kloud"
+    bucket         = "jenkins-backend678ghjk-kode-kloud"
     key            = "myapp/eks/tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform_state"
